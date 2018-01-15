ad311638778d77480bd2c3bbbb63f43d1cb14a8c
DDRAX76 <ddraxdev@gmail.com>
2018-01-15 17:01:56 +0100

    modal changes first version of modal

--- force-app/main/default/aura/OppSummaryItem/OppSummaryItemController.js
+++ force-app/main/default/aura/OppSummaryItem/OppSummaryItemController.js
@@ -1,3 +1,23 @@
-({
-   
+({    
+    handleShowModal: function(component, evt, helper) {
+        var selectedOppName = component.get("v.opp.Name");
+      
+        var modalBody;
+
+        $A.createComponent("c:OppSummaryItemDetails", {},
+                           function(content, status) {
+                               if (status === "SUCCESS") {
+                                   modalBody = content;
+                                   component.find('overlayLib').showCustomModal({
+                                       header: "Details Opportunity: " + selectedOppName,
+                                       body: modalBody, 
+                                       showCloseButton: true,
+                                       cssClass: "mymodal"
+                                       
+                                   })
+                                   
+                               }
+                               
+                           });
+    }
 })
